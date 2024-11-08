67fcf77639cb2a9561528712ea4d79f4c82d7965
Eman Mahmoud <emanalged@gmail.com>
2024-11-08 22:43:56 +0200

    check updated

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const forecast =require("./views/forecast");
 const country = process.argv[2];
 
 geocode( country , (error , data) => {
-    if(error){console.log("ERROR : " , error)}
-    else {console.log("DATA : "  , data)}
+    if(error){console.log("ERROR : " , error)
+    } else {
+        console.log("DATA : "  , data)
     forecast(data.latitude , data.longitude , (error , data) => {
         if(error){console.log("ERROR : " , error)}
         else {console.log("DATA : " , data)}
     })
+}
 })
